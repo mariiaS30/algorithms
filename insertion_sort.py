21f46400a1f7b61cfef49c4ff89b73fe75c62817
Mariia S <thimariia@gmail.com>
2023-10-16 22:05:53 -0700

    переделали алгоритм. осталось исправить баги

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,19 +1,30 @@
 a = [5, 6, 2, 0, 9, 1, 3]
+# a = [1, 3, 4, 5, 2]
 print(a)
 
+pointer = 0
 index = 0
-i = len(a) - 2
 for k in range(len(a)-1):
     now_number = a[-1]
-    while now_number < a[i]:
-        i = i-1
-    
-    index = i + 1
-    print(index)
+    # print(now_number)
+    index = pointer
+    for j in range(0, pointer + 1):
+        if now_number < a[j]:
+            index = j   
+    pointer = pointer + 1
+    # print(index)       
+ # print(f'i = {i}')
     for j in range(len(a)-1, index, -1):
         a[j] = a[j-1]
-    # a[index] = now_number    
-
-# print(a) 
+    a[index] = now_number
+    
+    print(now_number, index)
+    print(a)    
+    
+print(a) 
 
 #исправить ошибку с index
+# 1, 3, 4, 5, 2
+# i = 0
+# index = 0 
+# 5 2 4 7 3
