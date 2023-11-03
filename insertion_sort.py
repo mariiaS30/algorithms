ff6eea44e770ab7aba4b33d00f3a65b0b5dcd211
Mariia S <thimariia@gmail.com>
2023-11-02 21:55:23 -0700

    реализовали алгоритм сортировки вставками

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,30 +1,18 @@
-a = [5, 6, 2, 0, 9, 1, 3]
-# a = [1, 3, 4, 5, 2]
-print(a)
+a = ['a', 'b', 'g', 'h', 'c']
 
 pointer = 0
-index = 0
 for k in range(len(a)-1):
-    now_number = a[-1]
-    # print(now_number)
-    index = pointer
-    for j in range(0, pointer + 1):
-        if now_number < a[j]:
-            index = j   
+    now_number = a[pointer + 1]
+    index = pointer + 1   
+    j = 0
+    while now_number > a[j]:
+        j = j + 1   
+    index = j   
     pointer = pointer + 1
-    # print(index)       
- # print(f'i = {i}')
-    for j in range(len(a)-1, index, -1):
+   
+    for j in range(pointer, index, -1):
         a[j] = a[j-1]
     a[index] = now_number
-    
-    print(now_number, index)
-    print(a)    
-    
+       
 print(a) 
 
-#исправить ошибку с index
-# 1, 3, 4, 5, 2
-# i = 0
-# index = 0 
-# 5 2 4 7 3
